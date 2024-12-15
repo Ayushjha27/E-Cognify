e9fa94b66add297addef244c667e9c73350c0131
Ayushjha27 <76464855+Ayushjha27@users.noreply.github.com>
2024-12-15 19:40:35 +0530

    commited

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
- 
+
 import "./App.css";
 import { Button } from "./components/ui/button";
 import Login from './pages/Login';
@@ -6,11 +6,18 @@ import Login from './pages/Login';
 function App() {
   return (
     <main>
-     
-     <Login/>
+
+      <Login />
     </main>
   )
- 
+
 }
 
 export default App;
+
+
+// Database username -----AYuSH         ;          radha_krishna_1627
+
+
+//Connect to your MongoDB deployment.
+// mongodb+srv://AYuSH:<radha_krishna_1627>@cluster0.ldftd.mongodb.net/
